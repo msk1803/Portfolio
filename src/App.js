eac5d8e7f45967ddad4ce3288a180368080f6b6b
shubham <shubham.kumar1997@gmail.com>
2022-11-20 18:26:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './Components/Header/Header.jsx'
 import Nav from './Components/Nav/Nav.jsx'
 import About from './Components/About/About.jsx'
 import Experience from './Components/Experience/Experience.jsx'
-import Services from './Components/Services/Services.jsx'
+// import Services from './Components/Services/Services.jsx'
 import Portfolio from './Components/Portfolio/Portfolio.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 import Footer from './Components/Footer/Footer.jsx'
@@ -15,7 +15,7 @@ function App() {
       <Nav/>
       <About/>
       <Experience/>
-      <Services/>
+      {/* <Services/> */}
       <Portfolio/>
       <Contact/>
       <Footer/>
