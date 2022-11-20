d8ac44f06393c39e931559be438e7e535b9ec537
shubham <shubham.kumar1997@gmail.com>
2022-11-20 18:33:58 +0530

    finalUpdate

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import './Nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
+// import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageAltDots} from 'react-icons/bi'
 import {useState} from 'react'
 const Nav = () => {
@@ -14,7 +14,7 @@ const Nav = () => {
     <a href="#" onClick={()=> setActiveNav('#')}className={activeNav==='#'?'active':''}><AiOutlineHome/></a>
     <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav==='#about'?'active':''}><AiOutlineUser/></a>
     <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}><BiBook/></a>
-    <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav==='#services'?'active':''}><RiServiceLine/></a>
+    {/* <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav==='#services'?'active':''}><RiServiceLine/></a> */}
     <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}><BiMessageAltDots/></a>
   </nav>
   )
